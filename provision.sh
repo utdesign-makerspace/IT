e818f7f7830538b1ccb0aa333ec4c10d2e85fbe3
Cameron Steele <cam_steele@ymail.com>
2020-02-16 10:38:44 -0600

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir temp
+
+cd temp
+
 sudo apt update
 
 sudo apt install -y apt-transport-https curl chromium snapd ldap-auth-client nscd
@@ -10,10 +14,10 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt
 
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt install nodejs brave-browser -y
+curl https://files.multimc.org/downloads/multimc_1.4-1.deb -o multimc.deb 
+
+sudo apt install nodejs brave-browser ./multimc.deb -y
 
 sudo snap install spotify
 
-sudo auth-client-config -t nss -p lac_ldap
-
-curl https://files.multimc.org/downloads/multimc_1.4-1.deb -o multimc.deb 
+curl -s https://install.zerotier.com | sudo bash
